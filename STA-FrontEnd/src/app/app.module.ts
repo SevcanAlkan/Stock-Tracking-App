99688daf7ac86ce7f84bd7fe387dc86bb3852b62
Sevcan Alkan <sevdzhan.alkan@scalefocus.com>
2020-01-31 16:53:45 +0200

    Module import tested

--- STA-FrontEnd/src/app/app.module.ts
+++ STA-FrontEnd/src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Modules
+import { SharedModule } from './shared/shared.module'
+
+//Components
+
+
+//Services
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- STA-FrontEnd/src/app/shared/shared.module.ts
+++ STA-FrontEnd/src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { GridListComponent } from './components/grid-list/grid-list.component';
 
-
-
 @NgModule({
   declarations: [NavbarComponent, GridListComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    NavbarComponent,
+    GridListComponent
   ]
 })
 export class SharedModule { }
